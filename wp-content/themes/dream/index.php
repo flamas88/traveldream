6cbb8e742113e692d5877761ee9f4b198cf21616
Flavy Meniassom Massom <fmeniass@isimtl.local>
2016-02-16 11:46:15 -0500

    affichage des posts en page d'accueil

--- wp-content/themes/dream/index.php
+++ wp-content/themes/dream/index.php
@@ -1,6 +1,16 @@
 <?php get_header(); ?>
     <section>
         <!-- contenu des derniers articles-->
+
+       <?php while (have_posts()) : the_post(); ?>
+    <article>
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_excerpt(); ?></p>
+        <p>Article écrit par <?php the_author(); ?> en date de <?php the_date(''); ?></p>
+        <a href="<?php the_permalink(); ?>" class="more">Lire la suite</a>
+        <p><?php comments_popup_link('Aucun commentaire', '1 commentaire', '% commentaires'); ?></p>
+    </article>
+       <?php endwhile; ?>
     </section>
 
 <?php get_footer(); ?>
