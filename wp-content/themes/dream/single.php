7ab244faf83511c1aec3854c803c6d1af8c5482c
Flavy Meniassom Massom <fmeniass@isimtl.local>
2016-02-17 09:49:18 -0500

    ajout page single.php

--- wp-content/themes/dream/single.php
+++ wp-content/themes/dream/single.php
@@ -1,7 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: fmeniass
- * Date: 2016-02-16
- * Time: 08:48
- */
+<?php get_header(); ?>
+    <section>
+        <!-- contenu des derniers articles-->
+
+        <?php while (have_posts()) : the_post(); ?>
+            <article>
+                <h2><?php the_title(); ?></h2>
+                <p>Article écrit par <?php the_author(); ?> en date de <?php the_date(''); ?></p>
+                <p><?php the_content(); ?></p>
+            </article>
+        <?php endwhile; ?>
+    </section>
+<?php get_footer(); ?>
