e238877a0a3c49292922d4220aea6c27791afe27
Flavy Meniassom Massom <fmeniass@isimtl.local>
2016-02-17 09:36:57 -0500

    ajout pagination

--- wp-content/themes/dream/index.php
+++ wp-content/themes/dream/index.php
@@ -5,7 +5,7 @@
        <?php while (have_posts()) : the_post(); ?>
     <article>
         <h2><?php the_title(); ?></h2>
-        <p><?php the_excerpt(); ?></p>
+        <p><?php the_content(); ?></p>
         <p>Article écrit par <?php the_author(); ?> en date de <?php the_date(''); ?></p>
         <a href="<?php the_permalink(); ?>" class="more">Lire la suite</a>
         <p><?php comments_popup_link('Aucun commentaire', '1 commentaire', '% commentaires'); ?></p>
@@ -14,6 +14,11 @@
         <?php } ?>
     </article>
        <?php endwhile; ?>
+       <!-- instruction pour la pagination-->
+        <ul id="pagination">
+            <li><?php previous_posts_link('page précédente'); ?></li>
+            <li><?php next_posts_link('page suivante'); ?></li>
+        </ul>
     </section>
 
 <?php get_footer(); ?>
