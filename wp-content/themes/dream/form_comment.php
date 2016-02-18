4957baf5a9b5cadc3bf00c1120012c430b8968b3
Flavy Meniassom Massom <fmeniass@isimtl.local>
2016-02-18 09:29:56 -0500

    ajout pseudo dans le mode connexion

--- wp-content/themes/dream/form_comment.php
+++ wp-content/themes/dream/form_comment.php
@@ -1,15 +1,47 @@
 <?php
 // si les commentaires sont activés, on affiche le formulaire
 if('open' == $post->comment_status ): ?>
-<h3 id="respond">Lissez un commentaire</h3>
+<h3 id="respond">Laissez un commentaire</h3>
 <?php // si l'utilisateur est obligé d'être inscrit et qu'il n'est pas connecté,
 // on affiche un lien vers le formulaire de connexion
 
-if(/*get_option('comment_registration') &&*/ ! $user_ID) :
+if(get_option('comment_registration') && ! $user_ID) :
 ?>
 <p> vous devez être <a href="<?php get_option('siteurl')?>/wp-system/wp-login.php?redirect_to=<?php the_permalink(); ?>">connecté</a> pour laisser un commentaire. </p>
     <?php else: ?>
+    <form action="<?php get_option('siteurl'); ?>/wp-system/wp-comments-post.php" method="post">
 
-    <?php // Affichage du formulaire ici ?>
+        <?php if(is_user_logged_in()):
+            $current_user = wp_get_current_user(); ?>
+        <p> Connecté en tant que : <?php echo $current_user->display_name;  ?></p>
+        <?php //Ajouter liens de profil et lien de déconnexion ?>
+        <?php else: ?>
+       <p>
+            <input type="text" name="author" id="author" value="<?php $comment_author ?>" size="40" tabindex="1" placeholder="Votre nom"/>
+            <label for="author"> Nom <?php if($req) { echo "(requis)"; } ?></label>
+        </p>
+
+        <p>
+            <input type="text" name="email" id="email" value="<?php $comment_author_email ?>" size="40" tabindex="2" placeholder="exemple@courriel.com"/>
+            <label for="email"> Email <?php if($req) { echo "(requis)"; } ?></label>
+        </p>
+
+        <p>
+            <input type="text" name="url" id="url" value="<?php $comment_author_url ?>" size="40" tabindex="3" placeholder="htpp://"/>
+            <label for="url"> Site web </label>
+        </p>
+
+        <?php endif; ?>
+        <p>
+            <textarea name="comment" id="comment" cols="60" rows="10" tabindex="4"></textarea>
+        </p>
+
+        <p>
+        <input type="submit" name="submit" id="submit" tabindex="5" value="Envoyer"/>
+        <input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>"/>
+        </p>
+
+        </form>
+
     <?php endif; //Fin condition user est connecté?>
     <?php endif; //Fin condition "on peut laisser des commentaires" ?>
